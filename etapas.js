a7ab6d4dc389269e8c09e5a227b39f0dc80afd18
ozzy <ralfrr6@gmail.com>
2021-08-24 16:20:17 -0300

    urnaComplete

---
+++ etapas.js
@@ -0,0 +1,59 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: '38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {   
+                
+                numero: '77222',
+                nome: 'Beltrano da Silvia',
+                partido: 'DEFG', 
+                fotos: [
+                    {url: '77222.jpg', legenda: 'Vereador'}
+
+                ]
+            },
+        ]
+    },
+
+    {
+        
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                
+                numero: '99',
+                nome: 'CICLANO',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: '99.jpg', legenda: 'Prefeito'},
+                    {url: '84_2.jpg', legenda: 'vice-Prefeito', small: true}
+
+
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zuano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: '84.jpg', legenda: 'prefeito'},
+                    
+                    {url: '84_2.jpg', legenda: 'vice-Prefeito', small: true}
+                ] 
+            }
+        ]
+
+    }
+]
